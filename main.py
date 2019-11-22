a8462082a8474bfa5b9407a2983ebb7e5fa7e2b1
mannoone <olegs.letters@gmail.com>
2019-11-22 17:29:18 +0300

    Dialogflow detect_intent implementation in the bot

--- main.py
+++ main.py
@@ -6,10 +6,17 @@ from telegram.ext import Updater
 from telegram.ext import CommandHandler
 from telegram.ext import MessageHandler, Filters
 
+import dialogflow_v2
+
+
+logger = logging.getLogger('bot_logger')
 
 TELEGRAM_TOKEN = os.environ['TELEGRAM_VERBS_TOKEN']
 CHAT_ID = ContextVar('chat_id')
-logger = logging.getLogger('bot_logger')
+
+GOOGLE_APPLICATION_CREDENTIALS = os.environ['GOOGLE_APPLICATION_CREDENTIALS']
+DIALOGFLOW_PROJECT_ID = os.environ['DIALOGFLOW_PROJECT_ID']
+
 
 
 class BotHandler(logging.Handler):
@@ -22,15 +29,66 @@ class BotHandler(logging.Handler):
         self._bot.send_message(chat_id=CHAT_ID.get(), text=msg)
 
 
+class DialogflowHelper:
+    def __init__(self, chat_id, msg):
+        self._chat_id = chat_id
+        self._msg = msg
+
+        self._client = dialogflow_v2.SessionsClient()
+        self._session = self._client.session_path(DIALOGFLOW_PROJECT_ID, self._chat_id)
+        self._text_input = dialogflow_v2.types.TextInput(text=self._msg, language_code='ru')
+        self._query_input = dialogflow_v2.types.QueryInput(text=self._text_input)
+
+        self.response = self.detect_intent(self._client, self._session, self._query_input)
+
+    def detect_intent(self, client=None, session=None, query_input=None):
+        try:
+            response = client.detect_intent(session=session, query_input=query_input)
+        except:
+            logger.debug(f'Something goes wrong:\n', exc_info=True)
+            response = None
+
+        return response
+
+    @property
+    def query_text(self, response=None):
+        if response is None:
+            response = self.response
+
+        try:
+            result = response.query_result.query_text
+        except:
+            logger.debug('Cannot get the value of query_text:\n', exc_info=True)
+            result = None
+
+        return result
+
+    @property
+    def fulfillment_text(self, response=None):
+        if response is None:
+            response = self.response
+
+        try:
+            result = response.query_result.fulfillment_text
+        except:
+            logger.debug('Cannot get the value of fulfillment_text:\n', exc_info=True)
+            result = None
+        return result
+
+
 def start(update, context):
     CHAT_ID.set(update.effective_chat.id)
     message = 'Здравствуйте!'
     return context.bot.send_message(chat_id=update.effective_chat.id, text=message)
 
 
-def echo(update, context):
-    CHAT_ID.set(update.effective_chat.id)
-    context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+def greet(update, context):
+    chat_id = update.effective_chat.id
+    msg = update.message.text
+    CHAT_ID.set(chat_id)
+
+    df = DialogflowHelper(chat_id, msg)
+    context.bot.send_message(chat_id=chat_id, text=df.fulfillment_text)
 
 
 if __name__ == '__main__':
@@ -49,7 +107,7 @@ if __name__ == '__main__':
     start_handler = CommandHandler('start', start)
     dispatcher.add_handler(start_handler)
 
-    message_handler = MessageHandler(Filters.text, echo)
+    message_handler = MessageHandler(Filters.text, greet)
     dispatcher.add_handler(message_handler)
 
     while True:
